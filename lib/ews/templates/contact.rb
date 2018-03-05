64d580938e247d686f48793980a08a2353f80ce7
brianarpie <brian.arpie@gmail.com>
2018-03-05 17:02:34 -0500

    update contacts support complete

--- lib/ews/templates/contact.rb
+++ lib/ews/templates/contact.rb
@@ -27,7 +27,7 @@ module Viewpoint::EWS
             structure[:saved_item_folder_id] = { id: saved_item_folder_id }
           end
         end
-      
+
         structure[:items] = [{ contact: to_ews_item }]
         structure
       end
@@ -36,7 +36,7 @@ module Viewpoint::EWS
       #
       # Puts all known parameters in the required structure
       # @return [Hash]
-      def to_ews_item
+      def to_ews_item(opts={})
         item_parameters = {}
 
         PARAMETERS.each do |key|
@@ -50,31 +50,17 @@ module Viewpoint::EWS
 
             # Convert attributes
             case key
-            when :email_addresses
-              item_parameters[key] = value.each_with_index.map do |email, index|
-                item = { entry: {} }
-                item[:entry][:key] = "EmailAddress#{index + 1}"
-                item[:entry][:text] = email[:text] if email[:text].present?
-                item
-              end
             when :physical_addresses
-              item_parameters[key] = value.map do |address|
-                item = { entry: {} }
-                item[:entry][:key] = address[:key] if address[:key].present?
-                item[:entry][:street] = address[:street] if address[:street].present?
-                item[:entry][:city] = address[:city] if address[:city].present?
-                item[:entry][:state] = address[:state] if address[:state].present?
-                item[:entry][:country_or_region] = address[:country_or_region] if address[:country_or_region].present?
-                item[:entry][:postal_code] = address[:postal_code] if address[:postal_code].present?
-                item
+              if opts[:update]
+                item_parameters[key] ||= []
+                item_parameters[key] << set_physical_addresses_for_update(value)
+              else
+                item_parameters[key] = set_physical_addresses_for_create(value)
               end
+            when :email_addresses
+              item_parameters[key] = set_email_addresses(value, opts)
             when :phone_numbers
-              item_parameters[key] = value.map do |num|
-                item = { entry: {} }
-                item[:entry][:key] = num[:key] if num[:key].present?
-                item[:entry][:text] = num[:text] if num[:text].present?
-                item
-              end
+              item_parameters[key] = set_phone_numbers(value, opts)
             else
               item_parameters[key] = value
             end
@@ -83,6 +69,57 @@ module Viewpoint::EWS
 
         item_parameters
       end
+
+
+      private
+
+
+      def set_physical_addresses_for_update(address)
+        field = address.keys.first
+        {
+          entry: { 
+            Key: address[field][:key],
+            sub_elements: [{ field => { text: address[field][:text] } }]
+          }
+        }
+      end
+
+      def set_physical_addresses_for_create(addresses)
+        Array.wrap(addresses).map do |address|
+          item = { entry: {} }
+          item[:entry][:key] = address[:key] if address[:key].present?
+          item[:entry][:street] = address[:street] if address[:street].present?
+          item[:entry][:city] = address[:city] if address[:city].present?
+          item[:entry][:state] = address[:state] if address[:state].present?
+          item[:entry][:country_or_region] = address[:country_or_region] if address[:country_or_region].present?
+          item[:entry][:postal_code] = address[:postal_code] if address[:postal_code].present?
+          item
+        end
+      end
+
+      def set_phone_numbers(phone_numbers, opts)
+        Array.wrap(phone_numbers).map do |num|
+          item = { entry: {} }
+          item[:entry][get_key_name(opts)] = num[:key] if num[:key].present?
+          item[:entry][:text] = num[:text] if num[:text].present?
+          item
+        end
+      end
+
+      def set_email_addresses(emails, opts)
+        Array.wrap(emails).each_with_index.map do |email, index|
+          item = { entry: {} }
+          item[:entry][get_key_name(opts)] = email[:key]  if email[:key].present?
+          item[:entry][:text] = email[:text] if email[:text].present?
+          item
+        end
+      end
+
+      # NOTE: Capitalized Key required only when Updating
+      def get_key_name(opts={})
+        key_name = opts[:update] ? :Key : :key
+      end
+
     end
   end
 end
--- lib/ews/types/contact.rb
+++ lib/ews/types/contact.rb
@@ -38,9 +38,140 @@ module Viewpoint::EWS::Types
       office:          :office_location,
     }
 
+    # Updates the specified item attributes
+    #
+    # Uses `SetItemField` if value is present and `DeleteItemField` if value is blank
+    # @param updates [Hash] with (:attribute => value)
+    # @param options [Hash]
+    # @option options :conflict_resolution [String] one of 'NeverOverwrite', 'AutoResolve' (default) or 'AlwaysOverwrite'
+    def update_item!(attributes, opts={})
+      contact = {}
+      updates = map_attributes_to_updates(attributes)
+      contact[:item_changes] = [{ item_id: self.item_id, updates: build_updates(updates) }]
+      contact[:conflict_resolution] = opts[:conflict_resolution] || "AutoResolve"
+      update_contact(contact)
+    end
+
 
     private
 
+
+    def update_contact(data)
+      rm = ews.update_item(data).response_messages.first
+      if rm && rm.success?
+        self.get_all_properties!
+        self
+      else
+        raise EwsCreateItemError, "Could not update Contact. #{rm.code}: #{rm.message_text}"
+      end
+    end
+
+    def map_attributes_to_updates(attributes)
+      attributes.reduce([]) do |memo, (attribute, value)|
+        is_indexed = FIELD_URIS[attribute][:ftype] == :indexed_field_uRI rescue nil
+        if is_indexed
+          memo |= expanded_index_field(attribute, value)
+        else
+          memo << { attribute => value }
+        end
+        memo
+      end
+    end
+
+    def expanded_index_field(attribute, values)
+      Array.wrap(values).map do |value|
+        { attribute => value }
+      end
+    end
+
+    # EWS Item Updates Array
+    #
+    # Uses `SetItemField` if value is present and `DeleteItemField` if value is blank
+    # @return [Array]
+    def build_updates(updates)
+      updates.reduce([]) do |memo, update|
+        attribute, value = update.to_a.first # { attribute => value }
+
+        # Special Case for PhysicalAddresses
+        if attribute == :physical_addresses
+          update = build_physical_address_update(attribute, value)
+          memo << update if update
+          next memo
+        end
+
+        field_uri_name = attribute
+        field_index    = value[:key] rescue nil
+        item_field     = build_item_field(field_uri_name, field_index)
+
+        memo << get_item_field_update(item_field, attribute, value) if item_field
+        memo
+      end
+    end
+
+    def build_physical_address_update(attribute, value)
+      field_uri_name = value.keys.first rescue nil
+      field_index = value.values.first[:key] rescue nil
+
+      item_field = build_item_field(field_uri_name, field_index)
+      if item_field
+        get_item_field_update(item_field, attribute, value)
+      end
+    end
+
+    def build_item_field(field_uri_name, field_index=nil)
+      item_field = nil
+      if FIELD_URIS.include?(field_uri_name)
+        item_uri = FIELD_URIS[field_uri_name][:text]
+        item_ftype = FIELD_URIS[field_uri_name][:ftype] 
+      end
+      if item_uri
+        field_type = item_ftype || :field_uRI
+        item_field = { field_type => { field_uRI: item_uri } }
+        item_field[field_type].merge!({ field_index: field_index }) if field_index && field_type == :indexed_field_uRI
+      end
+      item_field
+    end
+
+    def get_item_field_update(field, attribute, value)
+      if is_blank_value(attribute, value)
+        { delete_item_field: field }
+      else
+        item = Viewpoint::EWS::Template::Contact.new(attribute => value)
+        { set_item_field: field.merge(contact: { sub_elements: map_sub_elements(item) }) }
+      end
+    end
+
+    # Remap attributes to sub elements because ews_builder #dispatch_field_item! uses #build_xml!
+    def map_sub_elements(item)
+      item.to_ews_item({ update: true }).map do |name, value|
+        map_sub_element(name, value)
+      end
+    end
+
+    def map_sub_element(name, value)
+      case value
+      when Hash
+        node = { name => {} }
+        value.each do |attrib_key, attrib_value|
+          attrib_key = camel_case(attrib_key) unless attrib_key == :text
+          node[name][attrib_key] = attrib_value
+        end
+        node
+      when String
+        { name => { text: value } }
+      when Array
+        { name => { sub_elements: value } }
+      else
+        { name => value }
+      end
+    end
+
+    def is_blank_value(attribute, value)
+      value.blank? ||
+        (attribute == :email_addresses && value[:text].blank?) ||
+        (attribute == :phone_numbers && value[:text].blank?) ||
+        (attribute == :physical_addresses && value.values.first[:text].blank?)
+    end
 
     def build_phone_number(phone_number)
       Types::PhoneNumber.new(ews, phone_number)
